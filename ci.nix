b9987d4887b3a3775404adf9f5eddf742ab99859
Eli Flanagan <eli@typedspace.com>
2021-11-12 16:29:51 -0500

    ci: reformat, add invocation

--- ci.nix
+++ ci.nix
@@ -1,19 +1,18 @@
-builtins.mapAttrs (k: _v:
-  let
-    url = "https://github.com/NixOS/nixpkgs/archive/2255f292063ccbe184ff8f9b35ce475c04d5ae69.tar.gz";
-    pkgs = import (builtins.fetchTarball url) { system = k; };
-  in
-  pkgs.recurseIntoAttrs {
-    # These two attributes will appear in your job for each platform.
-    hello = pkgs.hello;
-    cow-hello = pkgs.runCommand "cow-hello" {
-      buildInputs = [ pkgs.hello pkgs.cowsay ];
-    } ''
-      hello | cowsay > $out
-    '';
-  }
-) {
-  x86_64-linux = {};
-  # Uncomment to test build on macOS too
-  # x86_64-darwin = {};
+builtins.mapAttrs
+  (k: _v:
+    let
+      url = "https://github.com/NixOS/nixpkgs/archive/2fbba4b4416446721ebfb2e0bfcc9e45d8ddb4d2.tar.gz";
+      pkgs = import (builtins.fetchTarball url) { system = k; };
+    in
+    pkgs.recurseIntoAttrs {
+      execute-snippets = pkgs.runCommand "execute-snippets"
+        {
+          buildInputs = [ pkgs.nushell ];
+        } ''
+        nu ./scripts/run-all.nu > $out
+      '';
+    }
+  )
+{
+  x86_64-linux = { };
 }
